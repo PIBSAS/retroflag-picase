9ef85efb5be638bda56bc6b03c4bf01ed75c4c07
Luciano's tech <38385411+PIBSAS@users.noreply.github.com>
2025-06-21 12:35:24 -0300

    Update recalbox_install.sh

--- recalbox_install.sh
+++ recalbox_install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-SourcePath=https://raw.githubusercontent.com/RetroFlag/retroflag-picase/master
+#SourcePath=https://raw.githubusercontent.com/RetroFlag/retroflag-picase/master
+SourcePath=https://raw.githubusercontent.com/PIBSAS/retroflag-picase/master
 
 #make /boot writable---------------------------------
 sleep 2s
